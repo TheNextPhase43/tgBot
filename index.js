dd797ad49272a4edb9a8052962b5e597e4e9e3ba
TheNextPhase <thenextphase43@gmail.com>
2025-02-15 06:43:43 +0300

    Пара тройка функций, подписка на рассылку о новых видео, менюшка, базовые команды.

--- index.js
+++ index.js
@@ -38,8 +38,27 @@ var __generator = (this && this.__generator) || function (thisArg, body) {
 Object.defineProperty(exports, "__esModule", { value: true });
 var grammy_1 = require("grammy");
 var dotenv = require("dotenv");
+var fs = require("node:fs/promises");
 dotenv.config();
 var bot = new grammy_1.Bot(process.env.BOT_API_KEY);
+bot.api.setMyCommands([
+    {
+        command: "subscribe",
+        description: "subscribe to newest skibidi toilet videos",
+    },
+    {
+        command: "unsubscribe",
+        description: "unsubscribe",
+    },
+    {
+        command: "only_for_subs",
+        description: "only_for_subs",
+    },
+    {
+        command: "menu",
+        description: "open_menu",
+    },
+]);
 bot.command("start", function (ctx) { return __awaiter(void 0, void 0, void 0, function () {
     return __generator(this, function (_a) {
         switch (_a.label) {
@@ -50,7 +69,56 @@ bot.command("start", function (ctx) { return __awaiter(void 0, void 0, void 0, f
         }
     });
 }); });
-bot.hears("skibidi", function (ctx) { return __awaiter(void 0, void 0, void 0, function () {
+bot.command("menu", function (ctx) { return __awaiter(void 0, void 0, void 0, function () {
+    var menuKeyboard;
+    return __generator(this, function (_a) {
+        switch (_a.label) {
+            case 0:
+                menuKeyboard = new grammy_1.Keyboard().text("Ok").text("Med").text("bad");
+                return [4 /*yield*/, ctx.reply("Skibidi?", {
+                        reply_markup: menuKeyboard,
+                    })];
+            case 1:
+                _a.sent();
+                return [2 /*return*/];
+        }
+    });
+}); });
+bot.hears("123", function (ctx) { return __awaiter(void 0, void 0, void 0, function () {
+    return __generator(this, function (_a) {
+        ctx.reply("\u0422\u0435\u0441\u0442: <span class=\"tg-spoiler\">\u0421\u043A\u0438\u0431\u0438\u0434\u0438</span>", {
+            parse_mode: "HTML",
+        });
+        return [2 /*return*/];
+    });
+}); });
+bot.on(":photo").on("::url", function (ctx) { return __awaiter(void 0, void 0, void 0, function () {
+    return __generator(this, function (_a) {
+        ctx.reply("photo and url");
+        return [2 /*return*/];
+    });
+}); });
+bot.command("only_for_subs").filter(function (ctx) { return __awaiter(void 0, void 0, void 0, function () {
+    var subscribers;
+    return __generator(this, function (_a) {
+        switch (_a.label) {
+            case 0: return [4 /*yield*/, readSubscribers("./subscribersDatabase.json")];
+            case 1:
+                subscribers = _a.sent();
+                return [2 /*return*/, subscribers.includes(ctx.chatId.toString())];
+        }
+    });
+}); }, function (ctx) { return __awaiter(void 0, void 0, void 0, function () {
+    return __generator(this, function (_a) {
+        switch (_a.label) {
+            case 0: return [4 /*yield*/, ctx.reply("вы подписаны")];
+            case 1:
+                _a.sent();
+                return [2 /*return*/];
+        }
+    });
+}); });
+bot.command("skibidi", function (ctx) { return __awaiter(void 0, void 0, void 0, function () {
     return __generator(this, function (_a) {
         switch (_a.label) {
             case 0: 
@@ -61,10 +129,10 @@ bot.hears("skibidi", function (ctx) { return __awaiter(void 0, void 0, void 0, f
             // поэтому прилепил к краю строки
             return [4 /*yield*/, fetch("\nhttps://\nwww\n.googleapis\n.com\n/youtube\n/v3\n/search\n?part=snippet\n&channelId=".concat(process.env.DA_FUQ_BOOM_CHANNEL_ID, "\n&order=date\n&maxResults=1\n&key=").concat(process.env.YOUTUBE_DATA_API_V3_KEY, "\n"))
                     .then(function (response) { return response.json(); })
-                    .then(function (json) { return __awaiter(void 0, void 0, void 0, function () {
+                    .then(function (dataFromServer) { return __awaiter(void 0, void 0, void 0, function () {
                     return __generator(this, function (_a) {
                         switch (_a.label) {
-                            case 0: return [4 /*yield*/, ctx.reply("https://www.youtube.com/watch?v=".concat(json.items[0].id.videoId))];
+                            case 0: return [4 /*yield*/, ctx.reply("https://www.youtube.com/watch?v=".concat(dataFromServer.items[0].id.videoId))];
                             case 1: return [2 /*return*/, _a.sent()];
                         }
                     });
@@ -80,20 +148,158 @@ bot.hears("skibidi", function (ctx) { return __awaiter(void 0, void 0, void 0, f
         }
     });
 }); });
-bot.hears("пихаил чмо", function (ctx) { return __awaiter(void 0, void 0, void 0, function () { return __generator(this, function (_a) {
-    ctx.react("❤");
-    return [2 /*return*/];
-}); }); });
+function readSubscribers(path) {
+    return __awaiter(this, void 0, void 0, function () {
+        var currentSubscribersString, currentSubscribers;
+        return __generator(this, function (_a) {
+            switch (_a.label) {
+                case 0: return [4 /*yield*/, readFile(path)];
+                case 1:
+                    currentSubscribersString = _a.sent();
+                    if (currentSubscribersString.length) {
+                        currentSubscribers = JSON.parse(currentSubscribersString);
+                    }
+                    else {
+                        currentSubscribers = [];
+                    }
+                    // console.log(currentSubscribers);
+                    return [2 /*return*/, currentSubscribers];
+            }
+        });
+    });
+}
+function readFile(path) {
+    return __awaiter(this, void 0, void 0, function () {
+        return __generator(this, function (_a) {
+            switch (_a.label) {
+                case 0: return [4 /*yield*/, fs.readFile(path)];
+                case 1: return [2 /*return*/, (_a.sent()).toString()];
+            }
+        });
+    });
+}
+function writeFile(newData, path) {
+    return __awaiter(this, void 0, void 0, function () {
+        return __generator(this, function (_a) {
+            switch (_a.label) {
+                case 0: return [4 /*yield*/, fs.writeFile(path, JSON.stringify(newData), {
+                        encoding: "utf-8",
+                        flag: "w",
+                    })];
+                case 1:
+                    _a.sent();
+                    return [2 /*return*/];
+            }
+        });
+    });
+}
+// может быть вот это вот всё имеет смысл для оптимизации,
+// чтобы при каждом запросе к боту не читать заново файл, но
+// с другой стороны может быть это и бессмысленно
+// async function onLoadSubscribersRead() {
+//     // const currentlySavedSubscribers: string[] = await readSubscribers();
+//     // subscribers = new Set<string>(currentlySavedSubscribers);
+//     subscribers = new Set<string>(await readSubscribers());
+// }
+// let subscribers: Set<string>;
+// onLoadSubscribersRead();
+// const subscribers = new Set<number>([...currentlrySavedSubscribers]);
+bot.command("subscribe", function (ctx) { return __awaiter(void 0, void 0, void 0, function () {
+    var subscribers, _a;
+    return __generator(this, function (_b) {
+        switch (_b.label) {
+            case 0:
+                _a = Set.bind;
+                return [4 /*yield*/, readSubscribers("./subscribersDatabase.json")];
+            case 1:
+                subscribers = new (_a.apply(Set, [void 0, _b.sent()]))();
+                // в принципе можно добавить условие,
+                // но поскольку set хранит только
+                // уникальные значения, можно и
+                // не добавлять
+                subscribers.add(ctx.chatId.toString());
+                writeFile(Array.from(subscribers), "./subscribersDatabase.json");
+                return [2 /*return*/];
+        }
+    });
+}); });
+bot.command("unsubscribe", function (ctx) { return __awaiter(void 0, void 0, void 0, function () {
+    var subscribers, _a;
+    return __generator(this, function (_b) {
+        switch (_b.label) {
+            case 0:
+                _a = Set.bind;
+                return [4 /*yield*/, readSubscribers("./subscribersDatabase.json")];
+            case 1:
+                subscribers = new (_a.apply(Set, [void 0, _b.sent()]))();
+                subscribers.delete(ctx.chatId.toString());
+                writeFile(Array.from(subscribers), "./subscribersDatabase.json");
+                return [2 /*return*/];
+        }
+    });
+}); });
 // оставлю на момент, когда пихаил начнёт диалог с ботом,
 // чтобы засрать ему всё
 // setInterval(() => {
 //     bot.api.sendMessage(5646269039, "Я скибиди, а ты нет")
 // }, 1000);
+// тут будет проверка нет ли нового видео про скибиди туалеты
+// let latestVideoId: string;
+// (async function () {
+//     latestVideoId = await readFile("./latestVideoId.txt");
+// })();
+// setInterval(async () => {
+//     // отключил пока
+//     await fetch(`
+// https://
+// www
+// .googleapis
+// .com
+// /youtube
+// /v3
+// /search
+// ?part=snippet
+// &channelId=${process.env.DA_FUQ_BOOM_CHANNEL_ID}
+// &order=date
+// &maxResults=1
+// &key=${process.env.YOUTUBE_DATA_API_V3_KEY}
+// `)
+//         .then((response) => response.json())
+//         .then(async (dataFromServer) => {
+//             console.log(dataFromServer.items[0].id.videoId.toString());
+//             // отключено из за квоты, включить как восстановится ------------------------------------
+//             let latestVideoId = await readFile("./latestVideoId.txt");
+//             if (
+//                 dataFromServer.items[0].id.videoId.toString() !== latestVideoId
+//             ) {
+//                 latestVideoId = dataFromServer.items[0].id.videoId.toString();
+//                 await writeFile(
+//                     latestVideoId.toString(),
+//                     "./latestVideoId.txt"
+//                 );
+//                 const subscribers = await readSubscribers(
+//                     "./subscribersDatabase.json"
+//                 );
+//                 subscribers.forEach((el: string) => {
+//                     bot.api.sendMessage(
+//                         el,
+//                         `Новое видео про скибиди туалеты, срочно смотреть! https://www.youtube.com/watch?v=${latestVideoId}`
+//                     );
+//                 });
+//             }
+//         });
+// }, 60000);
+bot.hears("пихаил чмо", function (ctx) { return __awaiter(void 0, void 0, void 0, function () {
+    return __generator(this, function (_a) {
+        ctx.react("❤");
+        return [2 /*return*/];
+    });
+}); });
 // дефолтный ответ
 bot.on("message", function (ctx) { return __awaiter(void 0, void 0, void 0, function () {
     return __generator(this, function (_a) {
         switch (_a.label) {
-            case 0: return [4 /*yield*/, ctx.reply("skibidi ".concat(ctx.chatId))];
+            case 0: return [4 /*yield*/, ctx.reply("\u0422\u0435\u043F\u0435\u0440\u044C \u0443 \u043C\u0435\u043D\u044F \u0435\u0441\u0442\u044C \u0442\u0432\u043E\u0439 ID: ".concat(ctx.chatId, ".\n\u041E\u043D \u043E\u0441\u0442\u0430\u043D\u0435\u0442\u0441\u044F \u043A\u043E\u043D\u0444\u0438\u0434\u0435\u043D\u0446\u0438\u0430\u043B\u044C\u043D\u044B\u043C (\u043C\u043E\u0436\u0435\u0442 \u0431\u044B\u0442\u044C)."))];
             case 1:
                 _a.sent();
                 return [2 /*return*/];
--- index.ts
+++ index.ts
@@ -1,14 +1,61 @@
-import { Bot, GrammyError, HttpError } from "grammy";
+import { Bot, GrammyError, HttpError, Keyboard } from "grammy";
 import * as dotenv from "dotenv";
+import * as fs from "node:fs/promises";
 dotenv.config();
 
 const bot = new Bot(process.env.BOT_API_KEY!);
+
+bot.api.setMyCommands([
+    {
+        command: "subscribe",
+        description: "subscribe to newest skibidi toilet videos",
+    },
+    {
+        command: "unsubscribe",
+        description: "unsubscribe",
+    },
+    {
+        command: "only_for_subs",
+        description: "only_for_subs",
+    },
+    {
+        command: "menu",
+        description: "open_menu",
+    },
+]);
 
 bot.command("start", async (ctx) => {
     await ctx.reply(`Dop dop`);
 });
 
-bot.hears("skibidi", async (ctx) => {
+bot.command("menu", async (ctx) => {
+    const menuKeyboard = new Keyboard().text("Ok").text("Med").text("bad");
+    await ctx.reply("Skibidi?", {
+        reply_markup: menuKeyboard,
+    });
+});
+
+bot.hears("123", async (ctx) => {
+    ctx.reply(`Тест: <span class="tg-spoiler">Скибиди</span>`, {
+        parse_mode: "HTML",
+    });
+});
+
+bot.on(":photo").on("::url", async (ctx) => {
+    ctx.reply("photo and url");
+});
+
+bot.command("only_for_subs").filter(
+    async (ctx) => {
+        const subscribers = await readSubscribers("./subscribersDatabase.json");
+        return subscribers.includes(ctx.chatId.toString());
+    },
+    async (ctx) => {
+        await ctx.reply("вы подписаны");
+    }
+);
+
+bot.command("skibidi", async (ctx) => {
     // вообще по идее обратные кавычки позволяют
     // писать в несколько строк, а также они
     // должны удалять пробелы, и символы
@@ -30,16 +77,70 @@ www
 `)
         .then((response) => response.json())
         .then(
-            async (json) =>
+            async (dataFromServer) =>
                 await ctx.reply(
-                    `https://www.youtube.com/watch?v=${json.items[0].id.videoId}`
+                    `https://www.youtube.com/watch?v=${dataFromServer.items[0].id.videoId}`
                 )
         );
 });
 
-bot.hears("пихаил чмо", async (ctx) => {ctx.react("❤")});
+async function readSubscribers(path: string): Promise<string[]> {
+    const currentSubscribersString: string = await readFile(path);
 
+    let currentSubscribers: string[];
+    if (currentSubscribersString.length) {
+        currentSubscribers = JSON.parse(currentSubscribersString);
+    } else {
+        currentSubscribers = [];
+    }
 
+    // console.log(currentSubscribers);
+    return currentSubscribers;
+}
+
+async function readFile(path: string): Promise<string> {
+    return (await fs.readFile(path)).toString();
+}
+
+async function writeFile(newData: string[] | string, path: string) {
+    await fs.writeFile(path, JSON.stringify(newData), {
+        encoding: "utf-8",
+        flag: "w",
+    });
+}
+
+// может быть вот это вот всё имеет смысл для оптимизации,
+// чтобы при каждом запросе к боту не читать заново файл, но
+// с другой стороны может быть это и бессмысленно
+// async function onLoadSubscribersRead() {
+//     // const currentlySavedSubscribers: string[] = await readSubscribers();
+//     // subscribers = new Set<string>(currentlySavedSubscribers);
+//     subscribers = new Set<string>(await readSubscribers());
+// }
+// let subscribers: Set<string>;
+// onLoadSubscribersRead();
+
+// const subscribers = new Set<number>([...currentlrySavedSubscribers]);
+
+bot.command("subscribe", async (ctx) => {
+    const subscribers = new Set<string>(
+        await readSubscribers("./subscribersDatabase.json")
+    );
+    // в принципе можно добавить условие,
+    // но поскольку set хранит только
+    // уникальные значения, можно и
+    // не добавлять
+    subscribers.add(ctx.chatId.toString());
+    writeFile(Array.from(subscribers), "./subscribersDatabase.json");
+});
+
+bot.command("unsubscribe", async (ctx) => {
+    const subscribers = new Set<string>(
+        await readSubscribers("./subscribersDatabase.json")
+    );
+    subscribers.delete(ctx.chatId.toString());
+    writeFile(Array.from(subscribers), "./subscribersDatabase.json");
+});
 
 // оставлю на момент, когда пихаил начнёт диалог с ботом,
 // чтобы засрать ему всё
@@ -47,22 +148,65 @@ bot.hears("пихаил чмо", async (ctx) => {ctx.react("❤")});
 //     bot.api.sendMessage(5646269039, "Я скибиди, а ты нет")
 // }, 1000);
 
+// тут будет проверка нет ли нового видео про скибиди туалеты
+// let latestVideoId: string;
+// (async function () {
+//     latestVideoId = await readFile("./latestVideoId.txt");
+// })();
 
+// setInterval(async () => {
+//     // отключил пока
+//     await fetch(`
+// https://
+// www
+// .googleapis
+// .com
+// /youtube
+// /v3
+// /search
+// ?part=snippet
+// &channelId=${process.env.DA_FUQ_BOOM_CHANNEL_ID}
+// &order=date
+// &maxResults=1
+// &key=${process.env.YOUTUBE_DATA_API_V3_KEY}
+// `)
+//         .then((response) => response.json())
+//         .then(async (dataFromServer) => {
+//             console.log(dataFromServer.items[0].id.videoId.toString());
+//             // отключено из за квоты, включить как восстановится ------------------------------------
 
+//             let latestVideoId = await readFile("./latestVideoId.txt");
 
+//             if (
+//                 dataFromServer.items[0].id.videoId.toString() !== latestVideoId
+//             ) {
+//                 latestVideoId = dataFromServer.items[0].id.videoId.toString();
+//                 await writeFile(
+//                     latestVideoId.toString(),
+//                     "./latestVideoId.txt"
+//                 );
 
+//                 const subscribers = await readSubscribers(
+//                     "./subscribersDatabase.json"
+//                 );
+//                 subscribers.forEach((el: string) => {
+//                     bot.api.sendMessage(
+//                         el,
+//                         `Новое видео про скибиди туалеты, срочно смотреть! https://www.youtube.com/watch?v=${latestVideoId}`
+//                     );
+//                 });
+//             }
+//         });
+// }, 60000);
 
-
-
-
-
-
-
-
+bot.hears("пихаил чмо", async (ctx) => {
+    ctx.react("❤");
+});
 
 // дефолтный ответ
 bot.on("message", async (ctx) => {
-    await ctx.reply(`skibidi ${ctx.chatId}`);
+    await ctx.reply(`Теперь у меня есть твой ID: ${ctx.chatId}.
+Он останется конфиденциальным (может быть).`);
 });
 
 bot.catch((err) => {
